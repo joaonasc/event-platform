9a8dbd558724deed8369ba71a5a6783d7396f2db
João Nascimento <eujoao92@gmail.com>
2022-07-03 11:42:45 -0300

    Parte visual da tela de eventos

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,16 @@
-import {gql, useQuery} from "@apollo/client"
-import {useEffect} from "react"
-import { client } from "./lib/apollo"
+import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
+import {client} from "./lib/apollo";
+import { Router } from "./Router";
 
-const GET_LESSONS_QUERRY = gql`
-  query{
-    lessons {
-      id 
-      title
-    }
-  }
-`
-interface Lesson {
-  id: string;
-  title: string
-}
 
 function App() {
-
-  const {data} = useQuery<{lessons : Lesson[]}>(GET_LESSONS_QUERRY)
-  
-  // useEffect(() =>{
-  //   client.query({
-  //     query: GET_LESSONS_QUERRY,
-  //   }).then(response => {
-  //     console.log(response.data)
-  //   })
-  // },[] )
-
   return (
-  <ul>
-    {data?.lessons.map(lesson => {
-      return <li key={lesson.id}>{lesson.title}</li>
-    })}
-  </ul>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
   )
 }
 
