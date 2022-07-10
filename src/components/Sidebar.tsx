659b393d92638c27cef181301572c7fdb6950d07
João Nascimento <eujoao92@gmail.com>
2022-07-10 14:04:12 -0300

    Final das aulas IgniteLab

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,32 +1,13 @@
 import { useQuery, gql } from '@apollo/client';
-// import { useQuery } from 'react';
+import { useGetLessonsQuery } from '../graphql/generated';
 import { Lesson } from "./Lesson";
 
-const GET_LESSONS_QUERY = gql`
-  query  {
-  lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
-    id
-    lessonType
-    availableAt
-    title
-    slug
-  }
-}
 
-`
 
-interface GetLessonsQueryResponse {
-  lessons: {
-    id: string
-    title: string
-    slug: string
-    availableAt: string
-    lessonType: 'live' | 'class'
-  }[]
-}
+
 
 export function Sidebar() {
-  const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY)
+  const { data } = useGetLessonsQuery()
  
 
   return (
